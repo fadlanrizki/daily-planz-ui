a0f1cf6d02dfcc55b524d10e187ffce3f37b1009
Fadlan Rizki <66633252+fadlanrizki@users.noreply.github.com>
2025-01-20 00:16:18 +0700

    Create Logo Component

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ImageLogo from "../../assets/daily-planz-logo.png"
+
+const Logo = () => {
+  return <img className="block w-[100px] h-100px my-3 mx-5" src={ImageLogo} alt="logo" />;
+};
+
+export default Logo;
