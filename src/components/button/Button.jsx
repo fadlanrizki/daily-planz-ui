12bb2fec3c6c9e756f8f25912443607c761ba945
Fadlan Rizki <66633252+fadlanrizki@users.noreply.github.com>
2025-01-20 14:03:41 +0700

    change transition button behavior

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children, className, ...props }) => {
   return (
     <button
-      className={"rounded bg-primary py-2 px-4 text-sm text-white hover:bg-primary_hover active:bg-primary_hover"}
+      className={"rounded bg-primary py-2 px-4 text-sm text-white transition ease-in delay-75 hover:bg-primary_hover active:bg-primary_hover"}
       {...props}
     >
       {children}
